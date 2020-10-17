dfc47b9e8961249ea0984fccd7af3c641eb8618a
Ben-Callaghan <benccallaghan@gmail.com>
2020-10-17 17:56:29 +0100

    sldkfjsdlkfjs

--- game.js
+++ game.js
@@ -13,41 +13,11 @@ var player = {
 var round = 0;
 
 //var months = ["April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February", "March"];
-//
-//for (i=0, i<months.length, i++){
-//
-//}
 
-// <script>
-//      var seconds = 300;
-//var counter = document.getElementById("counter");
-//var myCounter = setInterval(function(){seconds--; counter.innerHTML = x}, 1000);
-//
-//    </script>
 
 //Functions
 function gameLoop(){
-// is it the first round y/n
-//y: prompt asset and discipline selection
-//n: what round is it?
 
-//show available cards
-//player picks card
-//<button onclick="clearInterval(myCounter)">Apply</button>
-// dice role based on player stats
-
-//did they get it?
-//y: congrats!
-//n: oh no! we had 9848483 applications, and on this occasion you were unsuccessful. We encourage you to try again next year and hope you'll sign up to our mailing list and follow us on facebook
-
-//Calculate effect of success/failure
-
-// does the success/failure prompt any other choices?
-//y: n:
-// great! Round over
-
-//was that the last round?
-//y: n:
 }
 
 
