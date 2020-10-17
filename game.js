258264fbda2b755ec16ac8e905cf8e993626a555
Gordon Douglas <gordondouglas@gordons-macbook.home>
2020-10-17 18:08:42 +0100

    these are sime changes

--- game.js
+++ game.js
@@ -7,12 +7,17 @@ var player = {
   name: "Tester",
   discipline: "Craft",
   location: "",
-  assets: ['one', 'two']
+  assets: ['one', 'two'],
+  local: ['4', '6', '8', '10', '12', '20'],
+  international: ['4', '6', '8', '10', '12', '20'],
+  craft: ['4', '6', '8', '10', '12', '20'],
+  academic: ['4', '6', '8', '10', '12', '20'],
+
 }
 
 var round = 0;
 
-//var months = ["April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February", "March"];
+var months = ["April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February", "March"];
 
 
 //Functions
