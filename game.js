e277818d80eedd9c46d7a4da006468a2f108d5dd
Ben-Callaghan <benccallaghan@gmail.com>
2020-10-17 13:38:11 +0100

    first commit - basic js and html

---
+++ game.js
@@ -0,0 +1,41 @@
+// SETUP
+
+// Vars
+var player = {
+  name: "",
+  discipline: "",
+  location: "",
+  assets: []
+}
+
+var round = 0;
+
+//Functions
+function gameLoop(){
+// is it the first round y/n
+//y: prompt asset and discipline selection
+//n: what round is it?
+
+//show available cards
+//player picks card
+// dice role based on player stats
+
+//did they get it?
+//y: congrats!
+//n: oh no!
+
+//Calculate effect of success/failure
+
+// does the success/failure prompt any other choices?
+//y: n:
+// great! Round over
+
+//was that the last round?
+//y: n:
+}
+
+
+//RUN TIME
+$(document).ready(function(){
+
+});
