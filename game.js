5de38a6214bb21f898851394975e1efffded9aba
Ben-Callaghan <benccallaghan@gmail.com>
2020-10-17 18:08:18 +0100

    added current month var

--- game.js
+++ game.js
@@ -12,12 +12,15 @@ var player = {
 
 var round = 0;
 
-//var months = ["April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February", "March"];
+var months = ["April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February", "March"];
 
+var currentMonth = months[0];
+console.log(months[0]);
+console.log(currentMonth);
 
 //Functions
 function gameLoop(){
-
+  $('.current-month')
 }
 
 
@@ -26,4 +29,7 @@ $(document).ready(function(){
   console.log('document is ready and game.js loaded correctly')
   console.log(player);
   $('.card_name').append(player.name);
+
+  // click next to do game loop
+  $('#next').click();
 });
